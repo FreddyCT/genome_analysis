700578f2d6f2a832893f5ebdafae89b00922738b
Chikiet Freddy Truong <chiki@pelle2.uppmax.uu.se>
2026-04-11 10:34:04 +0200

    edit 03_assembly.sh

---
+++ code/03_assembly.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 4
+#SBATCH -t 05:00:00
+#SBATCH -J canu_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=freddy.truong98@gmail.com
+#SBATCH --output=~/genome_analysis/analyses/02_assembly/%x.%j.out
+
+module load canu/2.3-GCCcore-13.3.0-Java-17
+
+mkdir -p ~/genome_analysis/analyses/02_assembly/canu/
+
+canu \
+    -p efaecium_e745 \
+    -d ~/genome_analysis/analyses/02_assembly/canu/ \
+    genomeSize=3m \
+    maxThreads=4 \
+    useGrid=false \
+    -pacbio ~/genome_analysis/data/raw_data/DNA/*.fastq.gz
