6e4fbe020021285d4cc307ae00030ec0a6434204
Chikiet Freddy Truong <chiki@pelle2.uppmax.uu.se>
2026-04-14 17:40:21 +0200

    split up serum and bhi runs

---
+++ code/01_trimming_BHI.sh
@@ -0,0 +1,32 @@
+  GNU nano 5.6.1                                                          01_trimming.sh
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 05:00:00
+#SBATCH -J trimmomatic
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x.%j.out
+
+module load Trimmomatic/0.39-Java-17
+
+INDIR=~/genome_analysis/data/raw_data/RNA
+OUTDIR=/proj/uppmax2026-1-61/nobackup/chiki/trimmed_data #Change directory to nobackup
+ADAPTERS=~/genome_analysis/data/adapters/TruSeq3-PE.fa
+
+mkdir -p $OUTDIR
+
+# BHI samples
+for sample in ERR1797972 ERR1797973 ERR1797974; do
+    echo "Processing $sample..."
+    trimmomatic PE -threads 2 \
+        $INDIR/${sample}_1.fastq.gz \
+        $INDIR/${sample}_2.fastq.gz \
+        $OUTDIR/${sample}_1_paired.fastq.gz \
+        $OUTDIR/${sample}_1_unpaired.fastq.gz \
+        $OUTDIR/${sample}_2_paired.fastq.gz \
+        $OUTDIR/${sample}_2_unpaired.fastq.gz \
+        ILLUMINACLIP:${ADAPTERS}:2:30:10 \
+        LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+    echo "Done with $sample"
+done
---
+++ code/01_trimming_serum.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 05:00:00
+#SBATCH -J trimmomatic
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x.%j.out
+
+module load Trimmomatic/0.39-Java-17
+
+INDIR=~/genome_analysis/data/raw_data/RNA
+OUTDIR=/proj/uppmax2026-1-61/nobackup/chiki/trimmed_data #Change directory to nobackup
+ADAPTERS=~/genome_analysis/data/adapters/TruSeq3-PE.fa
+
+mkdir -p $OUTDIR
+
+# First 3 = Serum samples
+for sample in ERR1797969 ERR1797970 ERR1797971; do
+    echo "Processing $sample..."
+    trimmomatic PE -threads 2 \
+        $INDIR/${sample}_1.fastq.gz \
+        $INDIR/${sample}_2.fastq.gz \
+        $OUTDIR/${sample}_1_paired.fastq.gz \
+        $OUTDIR/${sample}_1_unpaired.fastq.gz \
+        $OUTDIR/${sample}_2_paired.fastq.gz \
+        $OUTDIR/${sample}_2_unpaired.fastq.gz \
+        ILLUMINACLIP:${ADAPTERS}:2:30:10 \
+        LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+    echo "Done with $sample"
+done
